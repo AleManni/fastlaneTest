57220f6edf782f4ae0735db275794d6c4c149b86
Alessandro Manni <ale_manni@icloud.cm>
2019-03-31 16:45:41 +0100

    tests

--- fastlaneTestTests/fastlaneTestTests.swift
+++ fastlaneTestTests/fastlaneTestTests.swift
@@ -11,24 +11,23 @@ import XCTest
 
 class fastlaneTestTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  func testPresenter() {
+    let presenter = Presenter()
+    let spy = Spy()
+    presenter.displayable = spy
+    presenter.start()
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    XCTAssertEqual(spy.model.count, 20)
+  }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+}
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+class Spy: Displayable {
+
+  var model: [String] = []
+
+  func populate(with model: [String]) {
+    self.model = model
+  }
 
 }
